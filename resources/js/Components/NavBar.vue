bcf8bdb1ba873c61365374563a40b2ea0a81e7ca
lentrix <hawkmanlentrix@gmail.com>
2024-04-16 08:06:55 +0800

    init

---
+++ resources/js/Components/NavBar.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+
+    <section id="main-nav" class="bg-blue-600 flex items-center text-white">
+        <div id="brand" class="text-xl font-bold flex-1 p-3">
+            Midterm Removal
+        </div>
+        <nav>
+            <Link href="/">Home</Link>
+            <Link href="/products">Products</Link>
+            <Link href="/about">About</Link>
+        </nav>
+    </section>
+
+</template>
+
+<style scoped>
+
+a {
+    @apply p-4 hover:bg-blue-500;
+}
+
+</style>
---
+++ resources/js/Pages/Landing.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import NavBar from '@/Components/NavBar.vue'
+
+</script>
+
+<template>
+
+    <Head title="Landing Page" />
+
+    <NavBar />
+
+
+
+</template>
